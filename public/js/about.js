d55d87ee7c24a939eee36647b1b330f653491eab
mateocampillo <mateocampillo1@gmail.com>
2023-03-14 11:06:48 -0300

    about.js loa event changed to state ready change event

--- public/js/about.js
+++ public/js/about.js
@@ -1,35 +1,38 @@
-window.addEventListener('load', function() {
+document.onreadystatechange = () => {
+    if (document.readyState === "complete") {
+      
+        let about = document.querySelector('#about-a');
+        let aboutContainer = document.querySelector('#sectionAbout');
 
-    let about = document.querySelector('#about-a');
-    let aboutContainer = document.querySelector('#sectionAbout');
+        about.addEventListener('pointerdown', function() {
+            if(aboutContainer.classList.contains('claseAnimacionAbout')){
+                aboutContainer.classList.remove('claseAnimacionAbout')
+                aboutContainer.classList.add('claseAnimacionAboutOut')
+            } else {
+                aboutContainer.classList.remove('claseAnimacionAboutOut')
+                aboutContainer.classList.add('claseAnimacionAbout')
+            }
+        });
 
-    about.addEventListener('pointerdown', function() {
-        if(aboutContainer.classList.contains('claseAnimacionAbout')){
+        let crossAbout = document.querySelector('#cerrarAbout');
+
+        crossAbout.addEventListener('pointerdown', function() {
             aboutContainer.classList.remove('claseAnimacionAbout')
             aboutContainer.classList.add('claseAnimacionAboutOut')
-        } else {
+        });
+
+        let infoAbout = document.querySelector('#mainInfo-button');
+
+        infoAbout.addEventListener('pointerdown', function() {
             aboutContainer.classList.remove('claseAnimacionAboutOut')
             aboutContainer.classList.add('claseAnimacionAbout')
-        }
-    });
+        });
 
-    let crossAbout = document.querySelector('#cerrarAbout');
+        let englishButton = document.querySelector('#mainInfoIcons-button');
 
-    crossAbout.addEventListener('pointerdown', function() {
-        aboutContainer.classList.remove('claseAnimacionAbout')
-        aboutContainer.classList.add('claseAnimacionAboutOut')
-    });
+        englishButton.addEventListener('pointerdown', function() {
+            alert('Coming soon');
+        })
 
-    let infoAbout = document.querySelector('#mainInfo-button');
-
-    infoAbout.addEventListener('pointerdown', function() {
-        aboutContainer.classList.remove('claseAnimacionAboutOut')
-        aboutContainer.classList.add('claseAnimacionAbout')
-    });
-
-    let englishButton = document.querySelector('#mainInfoIcons-button');
-
-    englishButton.addEventListener('pointerdown', function() {
-        alert('Coming soon');
-    })
-})
+    }
+};
