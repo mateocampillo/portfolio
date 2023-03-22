5e5f847973f8776e674ed54ab48a2f379f96b4fd
mateocampillo <mateocampillo1@gmail.com>
2023-03-22 13:04:11 -0300

    alert for ecommerce

--- src/Components/Proyectos/Proyecto.js
+++ src/Components/Proyectos/Proyecto.js
@@ -2,12 +2,25 @@ import {
     ProyectoWrapperTheme,
     MainProyectoImageTheme
 } from '../Cover/Cover.elements';
+import Swal from 'sweetalert2';
 
 function Proyecto(props) {
+
+    const swalEcommerce = (e) => {
+        if(props.a === '#'){
+            e.preventDefault();
+            Swal.fire({
+                icon: 'info',
+                title: 'In development',
+                text: 'Coming soon.'
+                })
+            }
+        }
+    
     return (
             <ProyectoWrapperTheme>
-                <a href={props.a} target={'_blank'} rel="noreferrer"><MainProyectoImageTheme alt='Foto de proyecto' src={props.img}/></a>
-                <h3><a href={props.a} target={'_blank'} rel="noreferrer">{props.titulo}</a></h3>
+                <a href={props.a} target={'_blank'} rel="noreferrer" onPointerDown={swalEcommerce}><MainProyectoImageTheme alt='Foto de proyecto' src={props.img}/></a>
+                <h3><a href={props.a} target={'_blank'} rel="noreferrer" onPointerDown={swalEcommerce}>{props.titulo}</a></h3>
                 <p>{props.desc}</p>
             </ProyectoWrapperTheme>
     )
